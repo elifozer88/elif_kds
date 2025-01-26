8f9510d0619b8cc86fa47ab6b0efbe45f43def95
elifozer88 <150931308+elifozer88@users.noreply.github.com>
2025-01-26 15:43:33 +0300

    Update giris.js

--- public/js/giris.js
+++ public/js/giris.js
@@ -1,6 +1,5 @@
 document.getElementById('loginForm').addEventListener('submit', (event) => {
     event.preventDefault(); // Sayfanın yenilenmesini önler
 
-    // Kullanıcıyı direkt olarak yönlendir
     window.location.href = 'http://localhost:3000/pages/anasayfa.html';
 });
